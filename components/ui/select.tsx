f2f3d5c0b2e1c690a148300f22072c7cdc6064fa
boodev03 <ngocthang.devweb@gmail.com>
2025-02-18 20:30:15 +0700

    update form submit

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -20,7 +20,11 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex items-center justify-between gap-2",
+      "flex items-center justify-between py-[13px] px-[14px] h-[50px] w-full rounded-[4px] border border-neutral-border text-base text-neutral-primary-text",
+      "data-[disabled]:bg-neutral-background data-[disabled]:cursor-not-allowed",
+      "data-[placeholder]:text-[#C5C2C5]",
+      "focus:outline-none",
+      "bg-white hover:border-neutral-text-disable focus:border-neutral-primary-text",
       className
     )}
     {...props}
@@ -76,7 +80,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 w-[var(--radix-select-trigger-width)] overflow-hidden rounded-none border-x border-b border-neutral-border bg-white",
+        "relative z-50 w-[var(--radix-select-trigger-width)] overflow-hidden rounded-[4px] border border-neutral-border bg-white text-neutral-primary-text",
         "shadow-[0px_20px_40px_0px_rgba(0,0,0,0.1)]",
         "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2",
         position === "popper" && "data-[side=bottom]:translate-y-0",
